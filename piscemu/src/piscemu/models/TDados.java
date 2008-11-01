b2712e34ec734c71e879a5fb4f0f05f72626db3b
Thiago Elias <e.thiago.06@gmail.com>
2008-11-01 16:08:23 +0000

    inclusão de mais um construtor no TDados.
    ULA quase pronta, faltando as flags.
    
    Enviarei para a lista um arquivo de teste para ULA
    
    git-svn-id: http://piscemu.googlecode.com/svn/trunk@14 7c2f30b0-8451-11dd-82a6-aba1b78984bd

--- piscemu/src/piscemu/models/TDados.java
+++ piscemu/src/piscemu/models/TDados.java
@@ -11,6 +11,10 @@ public class TDados {
         this.dado = dado;
     }
     
+    public TDados(int dado){
+        this.dado = (short)dado;
+    }
+    
     public TDados(TDados dado){
         this.dado = dado.getDado();
     }
@@ -27,4 +31,49 @@ public class TDados {
         this.dado = dado.getDado();
     }
     
+    public TDados soma(TDados dado){
+        return new TDados((short)(this.dado+dado.getDado()));
+    }
+    
+    public TDados subtrai(TDados dado){
+        return new TDados((short)(dado.getDado() - this.dado));
+    }
+    
+    public TDados compara(TDados dado){
+        if(this.dado > dado.getDado())
+            return new TDados(1);
+        else if(this.dado < dado.getDado())
+            return new TDados(-1);
+        else
+            return new TDados(0);
+    }
+    
+    public TDados and(TDados dado){
+        return new TDados((short)(this.dado & dado.getDado()));
+    }
+    
+    public TDados or(TDados dado){
+        return new TDados((short)(this.dado | dado.getDado()));
+    }
+    
+    public TDados not(){
+        // fazer
+        return new TDados(~this.dado);
+    }
+    
+    public TDados zero(){
+        return new TDados(0);
+    }
+    
+    public TDados negacao(){
+        return new TDados((short)(-this.dado));
+    }
+    
+    public TDados shiftR(TDados dado){
+        return new TDados(this.dado>>dado.getDado());
+    }
+    
+    public TDados shiftL(TDados dado){
+        return new TDados(this.dado<<dado.getDado());
+    }    
 }
--- piscemu/src/piscemu/models/ULA.java
+++ piscemu/src/piscemu/models/ULA.java
@@ -10,7 +10,7 @@ package piscemu.models;
  * @author thiago
  */
 public class ULA {
-    public static final short QTDSinais = 5;
+    public static final short QTDSinais = 4;
     public static final short QTDFlags = 5;
     
     private BarramentoDados barramentoSaida;
@@ -27,7 +27,80 @@ public class ULA {
     }
     
     public void setSinal(boolean[] sinais){
+        short num = transformaSinais(sinais);
+        switch(num){
+            case 0:
+                barramentoSaida.setDados(barramentoEntradaA.getDados().soma(barramentoEntradaB.getDados()));
+                // a+b
+                break;
+            case 1:
+                barramentoSaida.setDados(barramentoEntradaA.getDados().subtrai(barramentoEntradaB.getDados()));
+                // b-a
+                break;
+            case 2:
+                barramentoSaida.setDados(barramentoEntradaA.getDados());
+                // a
+                break;
+            case 3:
+                barramentoSaida.setDados(barramentoEntradaA.getDados().compara(barramentoEntradaB.getDados()));
+                // a >=< b
+                break;
+            case 4:
+                barramentoSaida.setDados(barramentoEntradaA.getDados().and(barramentoEntradaB.getDados()));
+                // a and b
+                break;
+            case 5:
+                barramentoSaida.setDados(barramentoEntradaA.getDados().or(barramentoEntradaB.getDados()));
+                // a or b
+                break;
+            case 6:
+                barramentoSaida.setDados(barramentoEntradaA.getDados().not());
+                // not a
+                break;
+            case 7:
+                barramentoSaida.setDados(barramentoEntradaA.getDados().zero());
+                // 0
+                break;
+            case 8:
+                barramentoSaida.setDados(barramentoEntradaA.getDados().negacao());
+                // -a
+                break;
+            case 9:
+                barramentoSaida.setDados(barramentoEntradaA.getDados().shiftR(barramentoEntradaB.getDados()));
+                // b>>a
+                break;
+            case 10:
+                barramentoSaida.setDados(barramentoEntradaA.getDados().shiftL(barramentoEntradaB.getDados()));
+                // b<<a
+                break;
+            case 11:
+                barramentoSaida.setDados(barramentoEntradaA.getDados().shiftR(new TDados(1)));
+                // 1>>a
+                break;
+            case 12:
+                barramentoSaida.setDados(barramentoEntradaA.getDados().shiftL(new TDados(1)));
+                // 1<<a
+                break;
+        }
         
+    }
+    
+    private short transformaSinais(boolean[] sinais){
+        short ret = 0;
+        boolean t;
+        for(short i = 0; i < QTDSinais/2; i++){
+            t = sinais[i];
+            sinais[i] = sinais[QTDSinais-i-1];
+            sinais[QTDSinais-i-1] = t;
+        }
+        
+        for(short i = 0; i < QTDSinais; i++){
+            if(sinais[i]){
+                ret += Math.pow(2,i);
+            }
+        }
+        
+        return ret;
     }
 
 }
