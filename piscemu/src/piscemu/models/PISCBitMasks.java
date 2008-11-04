371c27b061f9629009f3f909b62843b184a775f1
Bruno Caricchio Buss <bruno.buss@gmail.com>
2008-11-04 16:46:43 +0000

    Mascaras binárias (constantes e estáticas) que serão utilizadas no Simulador.
    git-svn-id: http://piscemu.googlecode.com/svn/trunk@18 7c2f30b0-8451-11dd-82a6-aba1b78984bd

---
+++ piscemu/src/piscemu/models/PISCBitMasks.java
@@ -0,0 +1,13 @@
+/*
+ * Constantes para representar as máscaras binárias utilizadas.
+ */
+
+package piscemu.models;
+
+public class PISCBitMasks {
+
+    private static final int BMASK_END_ROTULO  = 0xFF00;
+    private static final int BMASK_END_INDICE  = 0x00FC;
+    private static final int BMASK_END_PALAVRA = 0x0003;
+    
+}
