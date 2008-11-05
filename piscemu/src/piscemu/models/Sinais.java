ae0bc0f71978e8497291d237648169d12b2d145e
Thiago Elias <e.thiago.06@gmail.com>
2008-11-05 10:57:33 +0000

    Uma classe para quardar as constantes de sinais da UC. Vou inserir umas mascaras(sem plágio), tipo SINAL_RA = SINAL_A, mas para isso preciso do arquivo de imagem da arquitetura, reformulada e com os sinais de controle.
    Davi, vc pode fazer isso?
    
    git-svn-id: http://piscemu.googlecode.com/svn/trunk@21 7c2f30b0-8451-11dd-82a6-aba1b78984bd

---
+++ piscemu/src/piscemu/models/Sinais.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package piscemu.models;
+
+/**
+ *
+ * @author thiago
+ */
+public class Sinais {
+    
+    public static final int SINAL_A = 0;
+    public static final int SINAL_B = 1;
+    public static final int SINAL_C = 2;
+    public static final int SINAL_D = 3;
+    public static final int SINAL_E = 4;
+    public static final int SINAL_F = 5;
+    public static final int SINAL_G = 6;
+    public static final int SINAL_H = 7;
+    public static final int SINAL_I = 8;
+    public static final int SINAL_J = 9;
+    public static final int SINAL_L = 10;
+    public static final int SINAL_M = 11;
+    public static final int SINAL_N = 12;
+    public static final int SINAL_O = 13;
+    public static final int SINAL_P = 14;
+    public static final int SINAL_Q = 15;
+    public static final int SINAL_R = 16;
+    public static final int SINAL_S = 17;
+    public static final int SINAL_T = 18;
+    public static final int SINAL_U = 19;
+    public static final int SINAL_V = 20;
+    public static final int SINAL_X = 21;
+//    public static final int SINAL_Z = 22;
+}
